bc5979045b40de85bbe003a2584d442cf9db92fd
Arvydas Sidorenko <asido4@gmail.com>
2012-02-27 08:40:12 +0100

    BUG FIX: libc strcmp function gave wrong results and even kernel panic

--- libc/libc.c
+++ libc/libc.c
@@ -283,7 +283,7 @@ static int cursor_move_line(int cnt)
  */
 int strcmp(const char* str1, const char* str2)
 {
-    while (*str1 == *str2 || *str1 != '\0' || *str2 != '\0')
+    while (*str1 == *str2 && (*str1 != '\0' || *str2 != '\0'))
     {
         str1++;
         str2++;
