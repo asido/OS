a95f9456467ce5a23903082a6331fe5c68a98bfe
Arvydas Sidorenko <asido4@gmail.com>
2012-04-14 22:56:44 +0200

    Running qemu as root? oO

--- qemu.sh
+++ qemu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ $1 = '-d' ]]; then
-	sudo qemu-system-i386 -fda FLOPPY.IMG -m 200 -S -gdb tcp::1234 -serial stdio
+	qemu-system-i386 -fda FLOPPY.IMG -m 200 -S -gdb tcp::1234 -serial stdio
 else
-	sudo qemu-system-i386 -fda FLOPPY.IMG -m 256
+	qemu-system-i386 -fda FLOPPY.IMG -m 256
 fi
